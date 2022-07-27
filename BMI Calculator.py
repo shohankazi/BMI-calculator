fb16d7c82b8c7daea43efebc71508bdf6d6693d3
shohan kazi <57486666+shohankazi@users.noreply.github.com>
2022-07-28 00:15:11 +0600

    Update BMI calculator

--- BMI Calculator.py
+++ BMI Calculator.py
@@ -1,6 +1,16 @@
 #----------------------BMI Calculator----------------
-user_weight = float(input("Enter your weight:"))
-user_height = float(input("enter your height:"))
-user_BMI = user_weight / ((user_height) * (user_height))
-print("Your BMI ",user_BMI)
+weight = input("enter your weight (kg): ")
+height = input("enter your height (m): ")
+BMI = float(weight) / (float(height) * float(height))
+print("your BMI ", round(BMI, 2))
+if BMI <= 18.5 :
+    print("You are underweight ")
+elif BMI <= 25 :
+    print("you have normal weight")
+elif BMI <= 30 :
+    print("You are overweight")
+elif BMI <= 35 :
+    print("you are obese")
+elif BMI > 35:
+    print("You are clinically obese")
 

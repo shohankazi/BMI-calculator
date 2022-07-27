bdda08f1f351e13e3e3c0b0f20c9621d5450cd32
Shohan Kazi <shohankazi012@gmail.com>
2022-07-27 08:59:33 +0600

    Code complete

---
+++ BMI Calculator.py
@@ -0,0 +1,6 @@
+#----------------------BMI Calculator----------------
+user_weight = float(input("Enter your weight:"))
+user_height = float(input("enter your height:"))
+user_BMI = user_weight / ((user_height) * (user_height))
+print("Your BMI ",user_BMI)
+
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# BMI-calculator
+
+# BMI Calculator
+
+I made this BMI Calculator using Python Programming language. 
+Here you can check your BMI(Body Mass Index) .
